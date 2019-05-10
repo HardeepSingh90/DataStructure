900a2d503c953714f29ebdf6f79ddde52353f04f
hardeep_singh <hardeep_singh@THBSGGNLPT-035.thbs.india.com>
2019-05-10 15:24:53 +0530

    Introduction

---
+++ src/dataStructure/DataStructure.java
@@ -0,0 +1,50 @@
+package dataStructure;
+
+public class DataStructure {
+	//-------------DATA STRUCTURE-------------------------------------------------
+	/*-->Data Structure is a way to represent relationship among different kind of data
+	 * and provide us set of operations to operate that data like insert, delete
+	 * , update ,searching and sorting.
+	 * 
+	 * -->In other words we can say that DATA Structure
+	 * deals with the study of how the data is organized in memory,how data can be 
+	 * retrieved and manipulate,possiblle ways how different data can be related
+	 * logically
+	 * 
+	 * ****************************************************************************/
+	
+	//--------SELECTING DATA STRUCTURE-----------------------------------------
+	/*analyze the problem -> determine the basic operation we have -> select Data Structure
+	 * ****************************************************************************/
+	
+	//----------TYPES of DATA StRUCTURE-------------------------------------------
+	/* 1)Primitive DS     :-int,float,char,Pointer..etc.. 
+	 * 2)Non Primitive DS :-Array,Structure,Stack
+	 * 3)Linear DS        :-Stack,queues,linked list
+	 * 4)Non Linear DS    :-trees & graphs
+	 * 
+	 * -->above all four types of DS are called as Abstract DS except Primitive DS.
+	 *    whenever we need to handle large amount of Data then we use abstract DS
+	 *    
+	 * 
+	 *-->Primitive DS is BUilt in DS and rest all are User Defined DS.   
+	 *    **************************************************************************/
+	
+	//-----------ALGORITHM-----------------------------------------------------------
+	/* a step by step procedure to solve a problem 
+	 * 
+	 * ******************************************************************************/
+	
+
+	
+	
+	//------------------------------------ARRAY---------------------------------------
+	/*Array normally used to store a related data or homogeneous data.
+	 *data stored in contiguous memory location in Array.
+	 *
+	 * -->Element :-Each item stored in array is called element.
+	 * -->Index   :-Each location of an element in array has an numeric index,which
+	 *              is used to identify the  element.
+	 *              
+	 * *******************************************************************************/
+}
