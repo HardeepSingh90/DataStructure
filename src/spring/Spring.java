977a0414c2845b4a63e320bc37f0267ab367ba5f
hardeep_singh <hardeep_singh@THBSGGNLPT-035.thbs.india.com>
2019-06-06 13:01:47 +0530

    basic about spring

---
+++ src/spring/Spring.java
@@ -0,0 +1,34 @@
+package spring;
+
+public class Spring {
+
+	
+	//---------------------what is Spring----------------
+	
+	/*-->spring is a framework in java.It can be thought of as a framework of frameworks because it provides support to various 
+	 * frameworks such as Struts, Hibernate, Tapestry, EJB, JSF etc.The Spring framework comprises several modules such as 
+	 * IOC, AOP, DAO, Context, ORM, WEB MVC etc.
+	 * 
+	 * ***************************************************************************************************/
+	
+	//-------------------Advantages of Spring Framework-----------------------
+	
+	/*-->Predefined Templates:-Spring framework provides templates for JDBC, Hibernate, JPA etc. technologies. So there is no need 
+	 * to write too much code. It hides the basic steps of these technologies.Let's take the example of JdbcTemplate, you don't need
+	 *  to write the code for exception handling, creating connection, creating statement, committing transaction, closing connection etc. 
+	 *  You need to write the code of executing query only. Thus, it save a lot of JDBC code
+	 *  
+	 *  -->Loose Coupling:-due to dependency injection spring framework provides loose coupling
+	 *  
+	 *  -->Easy to test:- as we don't need to write code like JDBC so we don't need to test that connectivity for code , we can simply
+	 *  believe it fine and working .LOL
+	 *  
+	 *  -->Lightweight:-Spring framework is lightweight because of its POJO implementation. The Spring Framework doesn't force the programmer 
+	 *       to inherit any class or implement any interface. That is why it is said non-invasive(Spring framework is said to be a non-invasive
+	 *        means it doesn't force a programmer to extend or implement their class from any predefined class or interface given by Spring API).
+	 *  -->Fast Development:-The Dependency Injection feature of Spring Framework and it support to various frameworks makes the easy development 
+	 *  of JavaEE application    
+	 *  -->Powerful abstraction:-It provides powerful abstraction to JavaEE specifications such as JMS, JDBC, JPA and JTA. 
+	 *  
+	 *   -->*/
+}
