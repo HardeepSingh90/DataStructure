63801c95c401722826f7759f4e4eff040d1e8951
hardeep_singh <hardeep_singh@THBSGGNLPT-035.thbs.india.com>
2019-08-14 15:31:16 +0530

    LinkedList Mechanism

---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,12 @@
+package linkedList;
+
+public class LinkedList {
+	
+	 Node head;
+	public void addOwn(int data)
+	{
+		Node node=new Node();
+		node.data=data;
+	}
+
+}
---
+++ src/linkedList/Runner.java
@@ -0,0 +1,10 @@
+package linkedList;
+
+public class Runner {
+	public static void main(String...s)
+	{
+		LinkedList list=new LinkedList();
+		list.addOwn(5);
+	}
+
+}
